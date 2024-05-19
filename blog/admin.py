b1faa1fb8e45d9b935075c27d5935e60d2e97a9f
haswanth reddy <v.haswanthreddy@gmail.com>
2024-05-20 00:47:17 +0530

    registered models from blog app to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Post
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ['name', 'username', 'address']
+    list_display = ['name', 'username']
+    search_fields = ['name']
+
+# can be used the above way with decorators or the below way, the one with decorators is more preffered 
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'author', 'description']
+    search_fields = ['title']
+
+
+admin.register(Post, PostAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100, null=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
